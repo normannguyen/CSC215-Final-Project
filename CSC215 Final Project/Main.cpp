247e7696aa36fc8864f136742ee12f9d47573cd4
KNOWN-UNIVERSE\nornguye <normannguyen3@gmail.com>
2019-04-26 11:12:12 -0700

    Add project files.

---
+++ CSC215 Final Project/Main.cpp
@@ -0,0 +1,18 @@
+//Norman Nguyen
+//Program: Learning Functions with Interactive Fiction using MULTIPLE FILES
+
+#include <iostream>
+#include <string>
+#include "Story.h"
+//Using NameSpace Standard Library
+using namespace std;
+//Main Function
+int main()
+{
+	//Welcome Message
+	WelcomeUser();
+	//Type Name
+	StoryTime();
+	//Pause
+	return 0;
+}
